c8cb94ef77654edb8ac33761829e17c31651f2f0
Babu <barbora.cervenkova@firma.seznam.cz>
2018-04-01 22:05:27 +0200

    5

--- src/main/java/name/babu/qooa/controller/HomeController.java
+++ src/main/java/name/babu/qooa/controller/HomeController.java
@@ -1,16 +1,9 @@
 package name.babu.qooa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import name.babu.qooa.language.LanguageService;
-import name.babu.qooa.model.Question;
 import name.babu.qooa.repository.QARepository;
 import name.babu.qooa.skin.SkinService;
 
@@ -28,16 +21,5 @@ public class HomeController {
     this.skin = skin;
   }
 
-  @RequestMapping("")
-  public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-                         @PageableDefault Pageable pageable,
-                         Model model) {
-    Page<Question> questions = qas.findAll(pageable);
-    model.addAttribute("message", name);
-    model.addAttribute("questions", questions);
-    model.addAttribute("lang", lang.getContext());
-    model.addAttribute("skin", skin.getSkin());
-    return "home";
-  }
 
 }
--- src/main/java/name/babu/qooa/controller/QAController.java
+++ src/main/java/name/babu/qooa/controller/QAController.java
@@ -1,12 +1,17 @@
 package name.babu.qooa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import name.babu.qooa.language.LanguageService;
+import name.babu.qooa.model.Question;
 import name.babu.qooa.repository.QARepository;
 import name.babu.qooa.skin.SkinService;
 
@@ -14,7 +19,7 @@ import name.babu.qooa.skin.SkinService;
  * XXX TODO create javadoc
  */
 @Controller
-@RequestMapping("questions")
+@RequestMapping("")
 public class QAController {
 
   private final QARepository qas;
@@ -28,17 +33,27 @@ public class QAController {
     this.skin = skin;
   }
 
-  @RequestMapping("{questionId}")
+  @RequestMapping("questions/{questionId}")
   public String questionDetail(@PathVariable long questionId, Model model) {
     model.addAttribute("question", qas.findOne(questionId));
     addContextInfo(model);
     return "question";
   }
 
-  @RequestMapping("ask")
-  public String ask(@PathVariable long questionId, Model model) {
+  @RequestMapping("")
+  public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+                         @PageableDefault Pageable pageable, Model model) {
+    Page<Question> questions = qas.findAll(pageable);
+    model.addAttribute("message", name);
+    model.addAttribute("questions", questions);
     addContextInfo(model);
-    return "question-ask";
+    return "home";
+  }
+
+  @RequestMapping("ask")
+  public String ask(Model model) {
+    addContextInfo(model);
+    return "ask-question";
   }
 
   private void addContextInfo(Model model) {
